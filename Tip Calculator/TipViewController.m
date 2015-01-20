6c5b49d7937ff520921dfb926b0c9c66c63d6987
Rupa Sharma <rsharma@greendotcorp.com>
2015-01-19 21:01:47 -0800

    Updated to reset billAmount to empty when clicked, added dollat sign to amount on, and added all data binding.

--- Tip Calculator/TipViewController.m
+++ Tip Calculator/TipViewController.m
@@ -9,10 +9,13 @@
 #import "TipViewController.h"
 
 @interface TipViewController ()
-@property (weak, nonatomic) IBOutlet UITextField *billAmountFIeld;
+@property (weak, nonatomic) IBOutlet UITextField *billAmountField;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipSelector;
+
+// causing the app to fail
+- (IBAction)onTap:(id)sender;
 
 @end
 
@@ -21,8 +24,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -30,4 +31,18 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+    [self updateValues];
+    [self.view endEditing:YES];
+}
+
+- (void)updateValues {
+    float billAmount = [[self.billAmountField.text stringByReplacingOccurrencesOfString:@"$" withString:@"" ] floatValue];
+    NSArray * tipValues = @[@(0.1), @(0.15), @(0.2)];
+    float tipAmount = billAmount * [tipValues[self.tipSelector.selectedSegmentIndex] floatValue];
+    float totalAmount = billAmount + tipAmount;
+    self.billAmountField.text = [NSString stringWithFormat:@"$%.2f", billAmount];
+    self.tipLabel.text =    [NSString stringWithFormat:@"$%.2f", tipAmount];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", totalAmount];
+}
 @end
